f8034677aed4250da663c273aad84c0ffabb53ed
astecenko@gmail.com <astecenko@gmail.com@a4166192-ec11-11de-8de6-035db03795fd>
2013-08-02 12:22:48 +0000

    В SAVLib_DBF.pas добавлен класс для создания таблиц с помощью TVKDBFNTX

--- SAVLib_DBF.pas
+++ SAVLib_DBF.pas
@@ -4,6 +4,28 @@ interface
 uses VKDBFDataSet, VKDBFNTX, VKDBFIndex, VKDBFSorters, Classes;
 type
   TFieldIndexArray = array[1..25] of Integer;
+
+  PDBFFielDefin = ^TDBFFielDefin;
+  TDBFFielDefin = record
+    FieldName: string;
+    FieldType: AnsiChar;
+    FieldLength: Word;
+  end;
+
+  TDBFFieldList = class(TList)
+  private
+    function Get(Index: Integer): PDBFFielDefin;
+  public
+    destructor Destroy; override;
+    function Add(Value: PDBFFielDefin): Integer;
+    function AddField(const FieldName: string; const FieldType: AnsiChar; const
+      FieldLength: Word): Integer;
+    function CreateTable(const DBFFileName: string): boolean;
+    function SaveToFile(const FileName: string; const ItemDelimiter: string =
+      ';'; const FieldDelimiter: string = #10#13): boolean;
+    property Items[Index: Integer]: PDBFFielDefin read Get; default;
+  end;
+
 procedure ClearStructDBF(Table1: TVKDBFNTX);
 procedure InitOpenDBF(Table1: TVKDBFNTX; const FileName: string; const
   AccessMode: Integer = 64; VersionDBF: xBaseVersion = xClipper;
@@ -68,64 +90,65 @@ var
   end;
 
 begin
-  if Table1.RecordCount=0 then Result:=False
-  else
-  begin
-  slist := TStringList.Create;
-  slist.Text := stringReplace(PoiskFields, ';', #13#10, [rfReplaceAll]);
-  HighNomer := IfThen(HighNo = 1, Table1.RecordCount, HighNo);
-  LowNomer := LowNo;
-  //функция AnsiCompareStr сравнивает 2 строки без учета регистра
-  // возвращает значение -1 если S1<S2, 0 если S1=S2, 1 если S1>S2
-  Table1.SetTmpRecord(LowNomer);
-  sKey := GetFieldSumm;
-  t := AnsiCompareText(sKey, PoiskKey);
-  if t > 0 then
+  if Table1.RecordCount = 0 then
     Result := False
-  else if t = 0 then
-  begin
-    Result := True;
-    Table1.CloseTmpRecord;
-    Table1.RecNo := LowNomer;
-  end
   else
   begin
-    Table1.SetTmpRecord(HighNomer);
+    slist := TStringList.Create;
+    slist.Text := stringReplace(PoiskFields, ';', #13#10, [rfReplaceAll]);
+    HighNomer := IfThen(HighNo = 1, Table1.RecordCount, HighNo);
+    LowNomer := LowNo;
+    //функция AnsiCompareStr сравнивает 2 строки без учета регистра
+    // возвращает значение -1 если S1<S2, 0 если S1=S2, 1 если S1>S2
+    Table1.SetTmpRecord(LowNomer);
     sKey := GetFieldSumm;
-    if AnsiCompareText(sKey, PoiskKey) < 0 then
+    t := AnsiCompareText(sKey, PoiskKey);
+    if t > 0 then
       Result := False
+    else if t = 0 then
+    begin
+      Result := True;
+      Table1.CloseTmpRecord;
+      Table1.RecNo := LowNomer;
+    end
     else
     begin
-      while (HighNomer - LowNomer) > 1 do
-      begin
-        mid := (LowNomer + HighNomer) div 2;
-        Table1.SetTmpRecord(mid);
-        sKey := GetFieldSumm;
-        if AnsiCompareText(sKey, PoiskKey) >= 0 then
-          HighNomer := mid
-        else
-          LowNomer := mid;
-      end;
-      Table1.SetTmpRecord(LowNomer);
+      Table1.SetTmpRecord(HighNomer);
       sKey := GetFieldSumm;
-      i := LowNomer;
       if AnsiCompareText(sKey, PoiskKey) < 0 then
-      begin
-        Table1.SetTmpRecord(HighNomer);
-        sKey := GetFieldSumm;
-        i := HighNomer;
-      end;
-      Table1.CloseTmpRecord;
-      if AnsiCompareText(sKey, PoiskKey) = 0 then
-      begin
-        Result := True;
-        Table1.RecNo := i;
-      end
+        Result := False
       else
-        Result := False;
+      begin
+        while (HighNomer - LowNomer) > 1 do
+        begin
+          mid := (LowNomer + HighNomer) div 2;
+          Table1.SetTmpRecord(mid);
+          sKey := GetFieldSumm;
+          if AnsiCompareText(sKey, PoiskKey) >= 0 then
+            HighNomer := mid
+          else
+            LowNomer := mid;
+        end;
+        Table1.SetTmpRecord(LowNomer);
+        sKey := GetFieldSumm;
+        i := LowNomer;
+        if AnsiCompareText(sKey, PoiskKey) < 0 then
+        begin
+          Table1.SetTmpRecord(HighNomer);
+          sKey := GetFieldSumm;
+          i := HighNomer;
+        end;
+        Table1.CloseTmpRecord;
+        if AnsiCompareText(sKey, PoiskKey) = 0 then
+        begin
+          Result := True;
+          Table1.RecNo := i;
+        end
+        else
+          Result := False;
+      end;
     end;
-  end;
-  FreeAndNil(slist);
+    FreeAndNil(slist);
   end;
 end;
 
@@ -154,61 +177,62 @@ var
   end;
 
 begin
-  if Table1.RecordCount=0 then Result:=False
-  else
-  begin
-  HighNomer := IfThen(HighNo = 1, Table1.RecordCount, HighNo);
-  LowNomer := LowNo;
-  //функция AnsiCompareStr сравнивает 2 строки без учета регистра
-  // возвращает значение -1 если S1<S2, 0 если S1=S2, 1 если S1>S2
-  Table1.SetTmpRecord(LowNomer);
-  sKey := GetFieldSumm;
-  t := AnsiCompareStr(sKey, PoiskKey);
-  if t > 0 then
+  if Table1.RecordCount = 0 then
     Result := False
-  else if t = 0 then
-  begin
-    Result := True;
-    Table1.CloseTmpRecord;
-    Table1.RecNo := LowNomer;
-  end
   else
   begin
-    Table1.SetTmpRecord(HighNomer);
+    HighNomer := IfThen(HighNo = 1, Table1.RecordCount, HighNo);
+    LowNomer := LowNo;
+    //функция AnsiCompareStr сравнивает 2 строки без учета регистра
+    // возвращает значение -1 если S1<S2, 0 если S1=S2, 1 если S1>S2
+    Table1.SetTmpRecord(LowNomer);
     sKey := GetFieldSumm;
-    if AnsiCompareStr(sKey, PoiskKey) < 0 then
+    t := AnsiCompareStr(sKey, PoiskKey);
+    if t > 0 then
       Result := False
+    else if t = 0 then
+    begin
+      Result := True;
+      Table1.CloseTmpRecord;
+      Table1.RecNo := LowNomer;
+    end
     else
     begin
-      while (HighNomer - LowNomer) > 1 do
-      begin
-        mid := (LowNomer + HighNomer) div 2;
-        Table1.SetTmpRecord(mid);
-        sKey := GetFieldSumm;
-        if AnsiCompareStr(sKey, PoiskKey) >= 0 then
-          HighNomer := mid
-        else
-          LowNomer := mid;
-      end;
-      Table1.SetTmpRecord(LowNomer);
+      Table1.SetTmpRecord(HighNomer);
       sKey := GetFieldSumm;
-      i := LowNomer;
       if AnsiCompareStr(sKey, PoiskKey) < 0 then
-      begin
-        Table1.SetTmpRecord(HighNomer);
-        sKey := GetFieldSumm;
-        i := HighNomer;
-      end;
-      Table1.CloseTmpRecord;
-      if AnsiCompareStr(sKey, PoiskKey) = 0 then
-      begin
-        Result := True;
-        Table1.RecNo := i;
-      end
+        Result := False
       else
-        Result := False;
+      begin
+        while (HighNomer - LowNomer) > 1 do
+        begin
+          mid := (LowNomer + HighNomer) div 2;
+          Table1.SetTmpRecord(mid);
+          sKey := GetFieldSumm;
+          if AnsiCompareStr(sKey, PoiskKey) >= 0 then
+            HighNomer := mid
+          else
+            LowNomer := mid;
+        end;
+        Table1.SetTmpRecord(LowNomer);
+        sKey := GetFieldSumm;
+        i := LowNomer;
+        if AnsiCompareStr(sKey, PoiskKey) < 0 then
+        begin
+          Table1.SetTmpRecord(HighNomer);
+          sKey := GetFieldSumm;
+          i := HighNomer;
+        end;
+        Table1.CloseTmpRecord;
+        if AnsiCompareStr(sKey, PoiskKey) = 0 then
+        begin
+          Result := True;
+          Table1.RecNo := i;
+        end
+        else
+          Result := False;
+      end;
     end;
-  end;
   end;
 end;
 
@@ -234,65 +258,147 @@ var
   end;
 
 begin
-if Table1.RecordCount=0 then Result:=False
-  else
-  begin
-  if HighNo = 1 then
-    HighNomer := Table1.RecordCount
-  else
-    HighNomer := HighNo;
-  LowNomer := LowNo;
-  //функция AnsiCompareStr сравнивает 2 строки без учета регистра
-  // возвращает значение -1 если S1<S2, 0 если S1=S2, 1 если S1>S2
-  Table1.SetTmpRecord(LowNomer);
-  sKey := GetFieldSumm;
-  t := AnsiCompareStr(sKey, PoiskKey);
-  if t > 0 then
+  if Table1.RecordCount = 0 then
     Result := False
-  else if t = 0 then
-  begin
-    Result := True;
-    Table1.CloseTmpRecord;
-    Table1.RecNo := LowNomer;
-  end
   else
   begin
-    Table1.SetTmpRecord(HighNomer);
+    if HighNo = 1 then
+      HighNomer := Table1.RecordCount
+    else
+      HighNomer := HighNo;
+    LowNomer := LowNo;
+    //функция AnsiCompareStr сравнивает 2 строки без учета регистра
+    // возвращает значение -1 если S1<S2, 0 если S1=S2, 1 если S1>S2
+    Table1.SetTmpRecord(LowNomer);
     sKey := GetFieldSumm;
-    if AnsiCompareStr(sKey, PoiskKey) < 0 then
+    t := AnsiCompareStr(sKey, PoiskKey);
+    if t > 0 then
       Result := False
+    else if t = 0 then
+    begin
+      Result := True;
+      Table1.CloseTmpRecord;
+      Table1.RecNo := LowNomer;
+    end
     else
     begin
-      while (HighNomer - LowNomer) > 1 do
-      begin
-        mid := (LowNomer + HighNomer) div 2;
-        Table1.SetTmpRecord(mid);
-        sKey := GetFieldSumm;
-        if AnsiCompareStr(sKey, PoiskKey) >= 0 then
-          HighNomer := mid
-        else
-          LowNomer := mid;
-      end;
-      Table1.SetTmpRecord(LowNomer);
+      Table1.SetTmpRecord(HighNomer);
       sKey := GetFieldSumm;
-      i := LowNomer;
       if AnsiCompareStr(sKey, PoiskKey) < 0 then
-      begin
-        Table1.SetTmpRecord(HighNomer);
-        sKey := GetFieldSumm;
-        i := HighNomer;
-      end;
-      Table1.CloseTmpRecord;
-      if AnsiCompareStr(sKey, PoiskKey) = 0 then
-      begin
-        Result := True;
-        Table1.RecNo := i;
-      end
+        Result := False
       else
-        Result := False;
+      begin
+        while (HighNomer - LowNomer) > 1 do
+        begin
+          mid := (LowNomer + HighNomer) div 2;
+          Table1.SetTmpRecord(mid);
+          sKey := GetFieldSumm;
+          if AnsiCompareStr(sKey, PoiskKey) >= 0 then
+            HighNomer := mid
+          else
+            LowNomer := mid;
+        end;
+        Table1.SetTmpRecord(LowNomer);
+        sKey := GetFieldSumm;
+        i := LowNomer;
+        if AnsiCompareStr(sKey, PoiskKey) < 0 then
+        begin
+          Table1.SetTmpRecord(HighNomer);
+          sKey := GetFieldSumm;
+          i := HighNomer;
+        end;
+        Table1.CloseTmpRecord;
+        if AnsiCompareStr(sKey, PoiskKey) = 0 then
+        begin
+          Result := True;
+          Table1.RecNo := i;
+        end
+        else
+          Result := False;
+      end;
     end;
-  end; 
   end;
+end;
+
+{ TDBFFieldList }
+
+function TDBFFieldList.Add(Value: PDBFFielDefin): Integer;
+begin
+  Result := inherited Add(Value);
+end;
+
+function TDBFFieldList.AddField(const FieldName: string;
+  const FieldType: AnsiChar; const FieldLength: Word): Integer;
+var
+  MyRec: PDBFFielDefin;
+begin
+  GetMem(MyRec, SizeOf(TDBFFielDefin));
+  MyRec.FieldName := FieldName;
+  MyRec.FieldType := FieldType;
+  MyRec.FieldLength := FieldLength;
+  Add(MyRec);
+end;
+
+function TDBFFieldList.CreateTable(const DBFFileName: string): boolean;
+var
+  Table0: TVKDBFNTX;
+  i, n: integer;
+begin
+  Table0 := TVKDBFNTX.Create(nil);
+  InitOpenDBF(Table0, DBFFileName, 18);
+  n := Count - 1;
+  for i := 0 to n do
+    with Table0.DBFFieldDefs.Add as TVKDBFFieldDef do
+    begin
+      Name := Self[i].FieldName;
+      field_type := Self[i].FieldType;
+      len := Self[i].FieldLength;
+    end;
+  Result := True;
+  try
+    Table0.CreateTable;
+  except
+    Result := False;
+  end;
+  FreeAndNil(Table0);
+end;
+
+destructor TDBFFieldList.Destroy;
+var
+  i, n: Integer;
+begin
+  n := Count - 1;
+  for i := 0 to n do
+    FreeMem(Items[i]);
+  inherited;
+end;
+
+function TDBFFieldList.Get(Index: Integer): PDBFFielDefin;
+begin
+  Result := PDBFFielDefin(inherited Get(Index));
+end;
+
+function TDBFFieldList.SaveToFile(const FileName, ItemDelimiter,
+  FieldDelimiter: string): boolean;
+var
+  List1: TStringList;
+  i, n: integer;
+  s: string;
+begin
+  s := '';
+  n := Count - 1;
+  Result := True;
+  List1 := TStringList.Create;
+  for i := 0 to n do
+    s := s + Self[i].FieldName + ItemDelimiter + Self[i].FieldType +
+      ItemDelimiter + IntToStr(Self[i].FieldLength) + FieldDelimiter;
+  List1.Text := s;
+  try
+    List1.SaveToFile(FileName);
+  except
+    Result := False;
+  end;
+  FreeAndNil(List1);
 end;
 
 end.
